e5c8a6aa785be027e7d5a91a2ecfc0fb12035e91
tim <startlingthings@gmail.com>
2011-06-12 01:29:42 -0500

    mosaic generates different sized versions!
    So far it does a "small" size for thumbnails and "large" for picture pages.
    Of course, the dimensions of these can be changed in the blogofile config.

--- _controllers/mosaic.py
+++ _controllers/mosaic.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from PIL import Image
 from blogofile.cache import bf
 
 config = {	
@@ -10,18 +11,32 @@ config = {
 			"path"			: 	"mosaic",
 			"dir"			:	"_photos",
 			"enable"		:	False,
+			"small"			:	(128, 128),
+			"large"			:	(400, 400),
 		}
 
 def run():
 	os.mkdir(os.path.join("_site", config['path']))
 	photos	= read_photos(config['dir'])
 	copy(photos)
+	resize(photos, config['small'], "small")
+	resize(photos, config['large'], "large")
 	index(photos)
 	photo_pages(photos)
 
 def copy(photos):
 	for photo in photos:
 		shutil.copy2(os.path.join(config['dir'], photo), os.path.join("_site",config['path'],photo))
+
+def resize(photos, size, destination):
+	os.mkdir(os.path.join("_site", config['path'], destination))
+	for photo in photos:
+		original	= os.path.join(config['dir'], photo)
+		to			= os.path.join("_site", config['path'], destination, photo)
+		file, ext 	= os.path.splitext(original)
+		im 			= Image.open(original)
+		im.thumbnail(size, Image.ANTIALIAS)
+		im.save(to)
 
 def read_photos(directory):
 	photos	= ([])
